5ea48c402fd03cd8f0b41561967d9fbe7ccec957
Dan Biscup <dabiscup@microsoft.com>
2017-05-10 15:09:05 -0600

    initial

---
+++ AJS2Demo1/Controllers/HomeController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+/* 
+Microsoft provides programming examples for illustration only, without 
+warranty either expressed or implied, including, but not limited to, the 
+implied warranties of merchantability and/or fitness for a particular 
+purpose. 
+　
+This sample assumes that you are familiar with the programming language 
+being demonstrated and the tools used to create and debug procedures. 
+Microsoft support professionals can help explain the functionality of a 
+particular procedure, but they will not modify these examples to provide 
+added functionality or construct procedures to meet your specific needs. 
+If you have limited programming experience, you may want to contact a 
+Microsoft Certified Partner or the Microsoft fee-based consulting line 
+at (800) 936-5200. 
+*/
+
+namespace AJS2Demo1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpPost]
+        public void Save(Model m)
+        {
+
+        }
+
+        public ActionResult GetBikes()
+        {
+            Model mReturn                                               = new Model();
+            try
+            {             
+                List<Bike> ab                                           = new List<Bike>();
+                ab.Add(new Bike { Name = "FSR", Make = "a", Price = 1M, Quantity = 10 });
+                ab.Add(new Bike { Name = "FSR", Make = "a", Price = 2M, Quantity = 10 });
+                ab.Add(new Bike { Name = "FSR", Make = "b", Price = 3M, Quantity = 10 });
+                ab.Add(new Bike { Name = "FSR", Make = "b", Price = 4M, Quantity = 10 });
+                ab.Add(new Bike { Name = "FSR", Make = "c", Price = 5M, Quantity = 10 });
+                ab.Add(new Bike { Name = "FSR", Make = "c", Price = 6M, Quantity = 10 });
+                ab.Add(new Bike { Name = "Sink'r Pro", Make = "c", Price = 2300M, Quantity = 5 });
+                mReturn.Bikes                                           = ab.ToArray();              
+            }
+            catch (Exception)
+            {
+                
+            }
+
+            return Json(mReturn, JsonRequestBehavior.AllowGet);            
+        }
+    }
+
+    public class Model
+    {
+        public Bike[] Bikes { get; set; }
+    }
+
+    public class Bike
+    {
+        public string Name { get; set; }
+        public string Make { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }                       
+    }
+}
---
+++ AJS2Demo1/Scripts/app/bikes.component.js
@@ -0,0 +1,134 @@
+﻿(function (app) {
+    app.BikesComponent =
+        ng.core.Component({
+            selector: 'bikes',
+            Providers: [ng.http.HTTP_PROVIDERS],
+            templateUrl: '/Scripts/app/bikes.component.html'
+        })
+            .Class({
+                constructor: [ng.http.Http, function (http) {
+                    this.http = http;
+
+                    this.Pages = [1,2,3,4];
+
+                    this.m_Model =
+                        {
+                            "Bikes": []
+                        };
+
+                    this.mapBikes().subscribe(function (result) {
+                        this.m_Model = result;
+                    }.bind(this));
+                }],
+                mapBikes: function () {
+                    return this.http.get('home/GetBikes').map(function (res) {
+                        return res.json();
+                    });
+                },
+                RemoveSpaces: function (value) {
+                    alert(value);
+                },
+                AddBike: function () {
+                    this.m_Model.Bikes.push({
+                        "Name": "",
+                        "Make": "",
+                        "Price": "0.00",
+                        "Quantity": 0
+                    });
+                },
+                DeleteBike: function (Bike) {
+                    var i = this.m_Model.Bikes.indexOf(Bike);
+                    this.m_Model.Bikes.splice(i, 1);
+                },
+                Save: function () {                   
+                    this.http.post('home/Save', this.m_Model, new Headers({ 'Content-Type': 'application/json' })).subscribe();
+                },
+                NameClick: function () {
+                    this.SortCol = "Name";
+
+                    if (this.SortOrder == "ASC")
+                        this.SortOrder = "DESC";
+                    else
+                        this.SortOrder = "ASC";                    
+                },
+                MakeClick: function () {
+                    this.SortCol = "Make";
+
+                    if (this.SortOrder == "ASC")
+                        this.SortOrder = "DESC";
+                    else
+                        this.SortOrder = "ASC";
+                },
+                PageClick: function (Page) {
+                    this.CurrentPage = Page;
+                },
+                PageUpdate(Length) {
+                    this.Pages = [];
+                    var nCount = 1;
+                    var nTotal = Length / 4;
+                    for (var i = 0; i < nTotal; i++) {
+                        this.Pages.push(nCount);
+                        nCount++;
+                    }
+                }
+            });
+
+    app.Pipe =
+        ng.core.Pipe({
+        name: 'mainpipe',
+        pure: false
+        })
+            .Class({
+                constructor: [function () {
+                    
+                }],
+                transform: function (items, name, make, price, sortcol, sortorder, page, com) {
+                    //console.log(sortcol + " " + sortorder);
+                    
+                    if (items && items.length)
+                    {
+                        //Build return
+                        var v = items.filter(item => {
+                            if (name && item.Name.toLowerCase().indexOf(name.toLowerCase()) === -1) {
+                                return false;
+                            }
+                            if (make && item.Make.toLowerCase().indexOf(make.toLowerCase()) === -1) {
+                                return false;
+                            }
+
+                            return true;
+                        });
+
+                        if (!page) {
+                            page = 0;
+                        }
+
+                        page = page * 4;
+                        var nStart = page;
+                        var sEnd = page + 4;
+
+                        com.PageUpdate(v.length);
+  
+                        //Sort
+                        if (!sortcol) {
+                            return v.sort(v => v.Name).slice(nStart, sEnd);
+                        }                        
+
+                        if (sortcol == "Name" && sortorder == "ASC")
+                            return v.sort(v => v.Name).slice(nStart, sEnd);
+                        else if (sortcol == "Name" && sortorder == "DESC")
+                        return v.sort(v => v.Name).reverse().slice(nStart, sEnd);
+                        else if (sortcol == "Make" && sortorder == "ASC")
+                            return v.sort(v => v.Make).slice(nStart, sEnd);
+                        else if (sortcol == "Make" && sortorder == "DESC")
+                            return v.sort(v => v.Make).reverse().slice(nStart, sEnd);
+                    }
+                    else
+                    {
+                        return items;
+                    }
+                }
+            });
+
+})(window.app || (window.app = {}));
+
